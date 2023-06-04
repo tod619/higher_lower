dba9094ab4e9c598bacc5eb3e7407c3b71ed2e41
tod619 <trevorodwyer619@hotmail.com>
2023-06-04 22:15:47 +0100

    generate random data for two accounts from game_data file in main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # Higher lower game
 # 02/06/2023
 from art import logo
-
+from game_data import data
+import random
 print(logo)
+
+account_a = random.choice(data)
+account_b = random.choice(data)
+print(account_a)
+print(account_b)
