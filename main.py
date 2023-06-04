5eed30c71a19656a572e88821084075da8b8a9a0
tod619 <trevorodwyer619@hotmail.com>
2023-06-04 22:24:46 +0100

    Create format_data function to format account data in main.py file

--- main.py
+++ main.py
@@ -3,9 +3,23 @@
 from art import logo
 from game_data import data
 import random
+
+
+def format_data(account):
+    """ Format the account data into a usuable form"""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    print(f"{account_name}, a {account_descr}, from {account_country}")
+
+
 print(logo)
 
 account_a = random.choice(data)
 account_b = random.choice(data)
-print(account_a)
-print(account_b)
+if account_a == account_b:
+    account_b = random.choice(data)
+
+
+format_data(account_a)
+format_data(account_b)
