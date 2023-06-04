968c956ed00bbdf2e05d544502da00053e3f321f
tod619 <trevorodwyer619@hotmail.com>
2023-06-04 23:13:04 +0100

    Make b the new a in each round in main.py file

--- main.py
+++ main.py
@@ -24,11 +24,12 @@ def check_answer(guess, a_followers, b_followers):
 print(logo)
 score = 0
 game_should_continue = True
+account_b = random.choice(data)
 
 while game_should_continue:
-    account_a = random.choice(data)
+    account_a = account_b
     account_b = random.choice(data)
-    if account_a == account_b:
+    while account_a == account_b:
         account_b = random.choice(data)
 
     print(f"Compare A: {format_data(account_a)}")
