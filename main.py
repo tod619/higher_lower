b51645740e4dd3ac80ac552045aa2aaeb336fedf
tod619 <trevorodwyer619@hotmail.com>
2023-06-04 23:04:12 +0100

    Make the game repeatable in main.py file

--- main.py
+++ main.py
@@ -23,24 +23,27 @@ def check_answer(guess, a_followers, b_followers):
 
 print(logo)
 score = 0
-account_a = random.choice(data)
-account_b = random.choice(data)
-if account_a == account_b:
+game_should_continue = True
+
+while game_should_continue:
+    account_a = random.choice(data)
     account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
 
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}")
 
-print(f"Compare A: {format_data(account_a)}")
-print(vs)
-print(f"Against B: {format_data(account_b)}")
+    guess = input("Who has more followers 'A' or 'B'?: ").lower()
 
-guess = input("Who has more followers 'A' or 'B'?: ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
-
-if is_correct:
-    score += 1
-    print(f"You are right!! Current Score: {score}")
-else:
-    print(f"You got it wrong! Final Scoe: {score}")
+    if is_correct:
+        score += 1
+        print(f"You are right!! Current Score: {score}")
+    else:
+        game_should_continue = False
+        print(f"You got it wrong! Final Scoe: {score}")
